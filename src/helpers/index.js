fc40a79acc7a2a93deb8f73ee46c0d3794b1e36d
karolisjon <karolisjonn@gmail.com>
2022-09-08 18:51:23 +0300

    feat(12): created router, controller and helper files.

---
+++ src/helpers/index.js
@@ -0,0 +1,22 @@
+const idGenerator = () => {
+  let id = '';
+  let length = 36;
+  let char_list =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    id += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  return id;
+};
+
+const correctProductDetails = ({ title, description, categoryId, price, img }) =>
+title !== undefined && typeof title === 'string' && title !== '' &&
+description !== undefined && typeof description === 'string' && description !== '' &&
+categoryId !== undefined && typeof categoryId === 'number' && categoryId !== '' &&
+price !== undefined && typeof price === 'number' && price > 0 &&
+img !== undefined && typeof img === 'string' && img !== '';
+
+module.exports = {
+  idGenerator,
+  correctProductDetails
+};
